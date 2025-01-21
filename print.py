5f6a0feab3f85f8198a97478378e051c020c11a3
Mohammad Kaif Iqbal <133042453+mkaifiqbal@users.noreply.github.com>
2025-01-21 13:13:24 +0530

    Update print.py
    new way to write

--- print.py
+++ print.py
@@ -1,1 +1,16 @@
-print("Hello World")
+//print("Hello World")
+str = "hello world"
+new=""
+
+for j in range(0,len(str)):
+    for i in range(97,123):
+        if str[j]==" ":
+            new+=" "
+            print(new)
+            break
+        elif chr(i)==str[j]:
+            new += str[j]
+            print(new)
+            break
+        else:
+            print(new+chr(i))
